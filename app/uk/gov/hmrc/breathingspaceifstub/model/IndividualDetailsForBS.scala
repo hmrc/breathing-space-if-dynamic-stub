fbad3631ff32ef8b6074cd0f47e3da241409c044
Lokesh Srinivasa <58558440+LokeshSrinivasa@users.noreply.github.com>
2021-01-14 16:28:08 +0000

    adding type fields to get inidividual details response

--- app/uk/gov/hmrc/breathingspaceifstub/model/IndividualDetailsForBS.scala
+++ app/uk/gov/hmrc/breathingspaceifstub/model/IndividualDetailsForBS.scala
@@ -23,9 +23,9 @@ import uk.gov.hmrc.breathingspaceifstub.repository.Individual
 object IndividualDetailsForBS {
 
   val Details = "details(nino,dateOfBirth)"
-  val NameList = "nameList(name(firstForename,secondForename,surname))"
+  val NameList = "nameList(name(firstForename,secondForename,surname,nameType))"
   val AddressList =
-    "addressList(address(addressLine1,addressLine2,addressLine3,addressLine4,addressLine5,addressPostcode,countryCode))"
+    "addressList(address(addressLine1,addressLine2,addressLine3,addressLine4,addressLine5,addressPostcode,countryCode,addressType))"
   val Indicators = "indicators(welshOutputInd)"
 
   val fields = s"$Details,$NameList,$AddressList,$Indicators"
