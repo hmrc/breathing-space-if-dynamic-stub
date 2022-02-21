de6436d8de2ed40c8b2a9a786d4493cda5242c78
tgeary90 <tgeary90@users.noreply.github.com>
2022-02-21 13:57:25 +0000

    BS-583: stubbed out test

--- it/uk/gov/hmrc/breathingspaceifstub/controller/UnderpaymentsControllerISpec.scala
+++ it/uk/gov/hmrc/breathingspaceifstub/controller/UnderpaymentsControllerISpec.scala
@@ -14,13 +14,28 @@
  * limitations under the License.
  */
 
-//package uk.gov.hmrc.breathingspaceifstub.controller
-//
-//import uk.gov.hmrc.breathingspaceifstub.support.BaseISpec
-//
-//class UnderpaymentsControllerISpec extends BaseISpec {
-//
-//  test("\"POST\" Underpayments should not allow duplicates") {
-//    postUnderpayments()
-//  }
-//}
+package uk.gov.hmrc.breathingspaceifstub.controller
+
+import play.api.http.Status.{CONFLICT, OK}
+import play.api.test.Helpers.status
+import uk.gov.hmrc.breathingspaceifstub.model.Underpayments
+import uk.gov.hmrc.breathingspaceifstub.support.BaseISpec
+
+class UnderpaymentsControllerISpec extends BaseISpec {
+
+  test("\"POST\" Underpayments should not allow duplicates") {
+
+    pending
+
+    // create some underpayments
+    val n1 = "AS000001A"
+    val p1 = "1519948e-8a54-11ec-8ed1-5bb13a0b0e94"
+    status(postUnderpayments(n1, p1, Underpayments(List(underpayment1)))) shouldBe OK
+
+    // push it again
+    val err = status(postUnderpayments(n1, p1, Underpayments(List(underpayment1))))
+
+    // check they 409 out (DUPLICATE)
+    err shouldBe CONFLICT
+  }
+}
