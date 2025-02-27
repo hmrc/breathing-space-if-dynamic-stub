c15aa84d02ca68af052f6aa135b3f7e571752da3
edmeck <edwin.meck@digital.hmrc.gov.uk>
2025-02-27 14:37:46 +0000

    wip

--- app/uk/gov/hmrc/breathingspaceifstub/controller/DebtsController.scala
+++ app/uk/gov/hmrc/breathingspaceifstub/controller/DebtsController.scala
@@ -54,9 +54,9 @@ class DebtsController @Inject() (debtsService: DebtsService, cc: ControllerCompo
     def jsonDataFromFile(filename: String): JsValue = getStaticJsonDataFromFile(s"debts/$filename")
     nino.take(8) match {
       case "AS000001" => Some(sendResponse(OK, jsonDataFromFile("singleBsDebtFullPopulation.json")))
-      case "AS000002" => Some(sendResponse(OK, jsonDataFromFile("singleBsDebtPartialPopulation.json")))
-      case "AS000003" => Some(sendResponse(OK, jsonDataFromFile("multipleBsDebtsFullPopulation.json")))
-      case "AS000004" => Some(sendResponse(OK, jsonDataFromFile("multipleBsDebtsPartialPopulation.json")))
+      case "AS000002" => Some(sendResponse(OK, jsonDataFromFile("multipleBsDebtsMixedPopulation.json")))
+//      case "AS000003" => Some(sendResponse(OK, jsonDataFromFile("singleBsDebtPartialPopulation.json")))
+//      case "AS000004" => Some(sendResponse(OK, jsonDataFromFile("multipleBsDebtsPartialPopulation.json")))
       case "AS000005" => Some(sendResponse(OK, jsonDataFromFile("multipleBsDebtsMixedPopulation.json")))
       case n if n.startsWith("BS") => Some(sendErrorResponseFromNino(n)) // a bad nino
       case _ => Some(sendResponse(NOT_FOUND, failures("RESOURCE_NOT_FOUND", "No records found for the given Nino")))
