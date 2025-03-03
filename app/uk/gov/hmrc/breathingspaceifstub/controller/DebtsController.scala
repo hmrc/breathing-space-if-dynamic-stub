356eeb2f843bac2fca65af5fde98b93ac173b897
edmeck <edwin.meck@digital.hmrc.gov.uk>
2025-03-03 15:21:26 +0000

    ATs running using breathing-space-if-dynamic-stub

--- app/uk/gov/hmrc/breathingspaceifstub/controller/DebtsController.scala
+++ app/uk/gov/hmrc/breathingspaceifstub/controller/DebtsController.scala
@@ -57,7 +57,7 @@ class DebtsController @Inject() (debtsService: DebtsService, cc: ControllerCompo
     nino.take(8) match {
       case "AS000001" => Some(sendResponse(OK, jsonDataFromFile("singleBsDebtFullPopulation.json")))
       case "AS000002" => Some(sendResponse(OK, jsonDataFromFile("multipleBsDebtsMixedPopulation.json")))
-//      case "AS000003" => Some(sendResponse(OK, jsonDataFromFile("singleBsDebtPartialPopulation.json")))
+      case "AS000003" => Some(sendResponse(OK, jsonDataFromFile("singleBsDebtPartialPopulation.json")))
 //      case "AS000004" => Some(sendResponse(OK, jsonDataFromFile("multipleBsDebtsPartialPopulation.json")))
       case "AS000005" => Some(sendResponse(OK, jsonDataFromFile("multipleBsDebtsMixedPopulation.json")))
       case n if n.startsWith("BS") => Some(sendErrorResponseFromNino(n)) // a bad nino
